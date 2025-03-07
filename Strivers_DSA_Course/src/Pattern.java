6b93bad1819d153c3d14e62780cad7fae8da9416
TirthNaik-99 <naik.ti@northeastern.edu>
2025-03-07 10:26:27 -0500

    Pattern 2 added

--- Strivers_DSA_Course/src/Pattern.java
+++ Strivers_DSA_Course/src/Pattern.java
@@ -7,25 +7,27 @@ public class Pattern {
         int value = scanner.nextInt();
         System.out.println("Enter the number of stars in one line : " + value);
         Patterns obj = new Patterns();
-        obj.print1(value);
+//        obj.print1(value);
+        obj.print2(value);
     }
 }
 
 class Patterns{
-    public void print1(int n){
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print("*");
-            }
-            System.out.println(" ");
-        }
-    }
+//    public void print1(int n){
+//
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+//    }
 
     public void print2(int n){
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
+
             }
             System.out.println(" ");
         }
