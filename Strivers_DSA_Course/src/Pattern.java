1b0e0e86b47dec0e2c7c1d8641d7df9c90a48913
TirthNaik-99 <naik.ti@northeastern.edu>
2025-03-07 11:47:42 -0500

    Updated pattern 4

--- Strivers_DSA_Course/src/Pattern.java
+++ Strivers_DSA_Course/src/Pattern.java
@@ -8,25 +8,47 @@ public class Pattern {
         System.out.println("Enter the number of stars in one line : " + value);
         Patterns obj = new Patterns();
 //        obj.print1(value);
-        obj.print2(value);
+//        obj.print2(value);
+//        obj.print3(value);
+        obj.print4(value);
     }
 }
 
 class Patterns{
-//    public void print1(int n){
-//
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < n; j++) {
-//                System.out.print("*");
-//            }
-//            System.out.println(" ");
-//        }
-//    }
+    public void print1(int n){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
 
     public void print2(int n){
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
+
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public void print3(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public void print4(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
 
             }
             System.out.println(" ");
